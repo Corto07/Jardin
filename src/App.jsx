50544f8cbed822a866ddb279547e2906a8fbebf9
Claudio <claudiocarruz@gmail.com>
2024-08-30 18:07:35 -0300

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Jardin de Infantes Martin Pescador</h1>
+      
+    </>
+  )
+}
+
+export default App
